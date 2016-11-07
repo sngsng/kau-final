e552931b53bf899954b94ff51279a46ba42b8d85
Seungjung <sngsng@slogup.com>
2016-11-07 09:25:43 +0900

    feat : Project settings

---
+++ ssing/ssing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ssing
+//
+//  Created by 김승중 on 2016. 11. 7..
+//  Copyright © 2016년 slogup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
